9baaf5ccde4a961ae764ace524ccfc449abc93d3
iagpomponet <iagosilbernagel@hotmail.com>
2021-01-07 21:19:06 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
+import { defaultTheme } from './theme';
+import { getPopularMovies } from './api/index';
+import GlobalStyles from './globalStyles';
+import Header from '../src/components/header/index';
+import Index from '../src/pages/index/index';
 
 function App() {
+  const [theme] = useState(defaultTheme);
+
+  const getMovies = async () => {
+    const latestMovies = await getPopularMovies();
+
+    console.log('latestMovies :>> ', latestMovies);
+  }
+
+  useEffect(() => {
+    getMovies();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyles />
+        <Header />
+        <Route exact path="/">
+          <Index />
+        </Route>
+      </Router>
+    </ThemeProvider>
   );
 }
 
